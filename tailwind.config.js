ea3f272647d3a5be87ecc7e866dc3d10636f73e6
Oussama Jaaouani <111194632+ntechnology0@users.noreply.github.com>
2022-10-16 18:37:20 +0100

    removing .env from git

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/core/**/*.{js,ts,jsx,tsx}",
+    "./app/auth/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
